d97d080f6654741143eba5a02680afcf826295b5
thepaul23 <88484882+thepaul23@users.noreply.github.com>
2021-08-06 12:16:47 +0300

    af

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const restaurant = {
+  name: `Classico Italiano`,
+  location: `Via Angelo Tavanati 23, Firenze, Italy`,
+  categories: [`Italian`, `Pizzeria`, `Vegetarian`, `Organic`],
+  starterMenu: [`Focaccia`, `Bruschetta`, `Garlic Bread`, `Caprese Salad`],
+  mainMenu: [`Pizza`, `Pasta`, `Risotto`],
+};
+
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+const [x, y, z] = arr;
